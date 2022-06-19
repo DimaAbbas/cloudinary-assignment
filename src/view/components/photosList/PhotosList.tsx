b3401e7000a4444847d33c95b94584426f60782d
Dima Abbas <dimaabbas25@gmail.com>
2022-06-19 21:41:56 +0300

    add axios method to get the photos from json-server

--- src/view/components/photosList/PhotosList.tsx
+++ src/view/components/photosList/PhotosList.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './PhotosList.scss';
 
 function PhotosList() {
+    const [photos, setPhotos] = useState([]);
+
+    useEffect(() => {
+        getPhotos();
+    }, []);
+
+    async function getPhotos() {
+        try {
+            const response = await axios.get(`http://localhost:3004/images`);
+            if (response) {
+                setPhotos(response.data);
+                console.log(response);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    //console.log(photos);
+
     return (
-        <div>
-            
+
+        <div className='PhotosList'>
+            {photos.map((photo: any, index: number) => {
+                return (
+                    <div key={index} className='photo'>
+                        <img src={photo.url} alt="hi" />
+                    </div>
+                );
+            })}
         </div>
     );
 }
